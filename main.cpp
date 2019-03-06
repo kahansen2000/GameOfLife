8e61551918c3033514409a82e99fcde17f9e6617
Katherine Hansen <13894347+kahansen2000@users.noreply.github.com>
2019-03-05 23:45:23 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
+//Katherine Hansen
+//2326665
+//kahansen@chapman.edu
+//CPSC350-01
+//Assignment 2: Game of Life
+
 #include <iostream>
 #include "PlayGame.h"
 
 using namespace std;
 
 int main(int argc, char** argv) {
-	PlayGame game;
-	game.PickMode();
-	return 0;
+        PlayGame game;
+        game.gameSettings();
+        return 0;
 };
