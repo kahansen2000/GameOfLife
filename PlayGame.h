4ed6ce93a3e2fba2c483c815d24bd7fabe4c6008
Katherine Hansen <13894347+kahansen2000@users.noreply.github.com>
2019-03-05 23:44:24 -0800

    Update PlayGame.h

--- PlayGame.h
+++ PlayGame.h
@@ -1,28 +1,36 @@
+//Katherine Hansen
+//2326665
+//kahansen@chapman.edu
+//CPSC350-01
+//Assignment 2: Game of Life
 #include <iostream>
-
+  
 using namespace std;
 
 class PlayGame{
-	public:
-		int PickMode();//lets the user choose their choice mode
-		void GameSettings();//Allows the user to choose their settings
-		
-		void FromFileBoard();//creates a board from the given file
-		void RandomBoard();//creates a randomized board
-		void PrintBoard();//prints the board
-		
-		int ClassicNeighbors(int x, int y);//returns num neighbors in classic mode
-		int DonutNeighbors(int x, int y);//returns num neighbors in donut mode
-		int MirrorNeighbors(int x, int y);//returns num neighbors in mirror mode
-		
-		int rows;
-		int cols;
-		int outputStyle;
-		string outputFile;
-		double density;
-		char** board;
-		char** tempBoard;
-		string inputFile;
-		int genCount;
-		int countCells;
+        public:
+                PlayGame();//constructor
+                ~PlayGame();//destructor
+
+                void  pickMode();//lets the user choose their choice mode
+                void gameSettings();//Allows the user to choose their settings
+
+                void fromFileBoard();//creates a board from the given file
+                void randomBoard();//creates a randomized board
+                void printBoard();//prints the board
+
+                int classicNeighbors(int x, int y);//returns num neighbors in classic mode
+                int donutNeighbors(int x, int y);//returns num neighbors in donut mode
+                int mirrorNeighbors(int x, int y);//returns num neighbors in mirror mode
+
+                int mode;
+                int outputStyle;
+                string outputFile;
+                double density;
+                char** board;
+                char** tempBoard;
+                string inputFile;
+                int genCount;
+                int countCells;
 };
+
